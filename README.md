06ddbad64c6e0ce0f7c2a348366a4ff6c4020d46
Fred Morrison <frederick.morrison@outlook.com>
2025-05-02 12:23:54 -0600

    add requirements.txt

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ A python Flask example with a MongoDB backend.
 |:----------|---------:|
 | Python    |   3.13.2 |
 | Flask     |    3.1.0 |
-| VSCode    |   1.97.2 |
-| PyCharm   | 2024.3.3 |
 | PyODMongo |    1.5.1 |
 | Pydantic  |   2.10.6 |
 | Pymongo   |   4.11.1 |
 | Motor     |    3.7.0 |
+| PyCharm   | 2024.3.3 |
+| VSCode    |   1.97.2 |
 
 ## Change History
 
--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import sys
-from routes import bp
-from flask import Flask, jsonify
-from pymongo import MongoClient
+from importlib.metadata import version
 
-from program_settings import ProgramSettings
+from flask import Flask
+
+from routes import bp
+
+
+
+
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+
+def get_package_version(package_name: str) -> str:
+    return version(package_name)
 
 app = Flask(__name__)
-# route information is in a separate file, so use Blueprint feature of Flask
-app.register_blueprint(bp)
+print(f'Python version: {get_python_version()}')
+print(f'pymongo version: {get_package_version("pymongo")}')
+print(f'python-dotenv version: {get_package_version("python-dotenv")}')
+print(f'pathlib version: {get_package_version("pathlib")}')
+
+
+def main():
+    # route information is separate, so use Blueprint feature of Flask
+    app.register_blueprint(bp)
+    app.run(debug = True)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    main()
